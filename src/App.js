3c980dcb844563ad9ca380c1967dd4d5e3dc434b
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-05-25 11:39:21 +0200

    Navbar functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from "react-router-dom";
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <header>
+
+      </header>
+      <body>
+
+      </body>
+      <Navbar />
+      <Switch>
+        {/* <Route path="/sign" component={PatientSignup} />
+        <Route path="/schedule" component={DoctorSchdule} />
+        <Route path="/database" component={PatientDatabase} />
+        <Route exact path="/" component={HomePage} /> */}
+      </Switch>
     </div>
   );
 }
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import { NavLink } from "react-router-dom";
+
 
 export default function Navbar() {
     return (
         <div>
-            
+            <nav>
+                <ul>
+                    <li><NavLink to="/">Home</NavLink></li>
+                    <li><NavLink to="/schedule">Doctor Schedule</NavLink></li>
+                    <li><NavLink to="/signup">Patient Signup</NavLink></li>
+                    <li><NavLink to="/database">Patient Database</NavLink></li>
+                </ul>
+            </nav>
         </div>
     )
 }
