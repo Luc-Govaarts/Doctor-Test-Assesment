9b091268c57e4f5fa996eb3c50558de2cac71289
Luc-Govaarts <lucgovaarts@hotmail.com>
2020-05-25 12:37:51 +0200

    Made contact. opening time component dinamic

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export default function Contact() {
+    var today = new Date(); 
+    console.log("Testing getHours:", today.getHours())
+    function openOrClosed() {
+        if(today.getHours() >= 17 || today.getHours() < 8) {
+            return "Closed"
+        } else {
+            return "open"
+        }
+    }   
+
     return (
         <div>
-            
+           <p>We are: <em>{openOrClosed()}</em></p> 
+           <p>To make an appointment</p>
+           <p>call: 020 555 5555</p>
         </div>
     )
 }
